563cbff8b88f905c122af1b90b27998b5c92883b
EqmalZahri <84484647+EqmalZahri@users.noreply.github.com>
2021-08-03 21:01:58 +0800

    Update CustomerLoginServlet.java

--- src/foxtrotCustomer/web/CustomerLoginServlet.java
+++ src/foxtrotCustomer/web/CustomerLoginServlet.java
@@ -53,7 +53,7 @@ public class CustomerLoginServlet extends HttpServlet {
 			else
 			{
 				request.setAttribute("WrongLoginMsg", authorize); // wrong login error message
-				RequestDispatcher rd = request.getRequestDispatcher("InvalidLogin.jsp"); // show error in the page
+				RequestDispatcher rd = request.getRequestDispatcher("CustomerLogin.jsp"); // show error in the page
 				rd.include(request, response);
 			}
 		}
